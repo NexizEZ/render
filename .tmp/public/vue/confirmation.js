30271094450cec48b385de817d777b483231cc03
ti392sch <ti392sch@htwg-konstanz.de>
2023-05-26 19:37:47 +0200

    additional adjsutments html

--- .tmp/public/vue/confirmation.js
+++ .tmp/public/vue/confirmation.js
@@ -10,10 +10,19 @@ export default {
   
     },
   template: `
-    <div class="container h2 my-10 d-flex justify-content-center" style="height: 500px;">
-    <div class="d-flex justify-content-center">
-      Bestellung erfolgreich.
+  <div class="container my-5">
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-8 col-lg-6 text-center">
+      <h1 class="mb-4"> Vielen Dank! Bestellung erfolgreich!</h1>
+      <hr>
+      <h3 class="mb-4">
+        Klicke hier, um deine Bestellungen einzusehen oder zu stornieren.
+      </h3>
+      <div class="d-grid">
+        <a href="/account" class="btn btn-primary button-17">Mein Account</a>
       </div>
     </div>
+  </div>
+</div>
       `,
 };
